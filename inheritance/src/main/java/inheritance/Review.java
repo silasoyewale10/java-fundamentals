da21041e8fe839dd0d600a0b583413e10ea4f234
Silas Oyewale <silasoyewale10@gmail.com>
2020-01-15 10:23:58 -0800

    lab 6 restaurant and review all tests passed

--- inheritance/src/main/java/inheritance/Review.java
+++ inheritance/src/main/java/inheritance/Review.java
@@ -1,13 +1,13 @@
 package inheritance;
 
-public class Review extends Restaurant {
+public class Review  {
     //defaults contructor calls Restaurant
     String body;
     String author;
     int numOfStars;
 
-    public Review (String body, String author, int numOfStars, String name, int stars, String priceCategory) {
-        super(name, stars, priceCategory);
+    public Review (String body, String author, int numOfStars) {
+//        super(name, stars, priceCategory);
         this.body = body;
         this.author = author;
         this.numOfStars = numOfStars;
--- inheritance/src/test/java/inheritance/RestaurantTest.java
+++ inheritance/src/test/java/inheritance/RestaurantTest.java
@@ -1,12 +1,42 @@
 package inheritance;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 public class RestaurantTest {
-    @Test
-    public void testRestaurantConstructor() {
+    @Test public void testRestaurantConstructor() {
         Restaurant test1 = new Restaurant("Ghu Kaku", 4, "$$");
         System.out.println(test1.toString());
-//        assertEquals();
+        String name = test1.name;
+        int star = test1.numOfStars;
+        String pricecategory = test1.priceCategory;
+        assertEquals("Ghu Kaku", test1.name);
+        assertEquals(4, test1.numOfStars);
+        assertEquals("$$", test1.priceCategory);
     }
+    @Test public void testRestaurantToString() {
+        Restaurant test1 = new Restaurant("Ghu Kaku", 4, "$$");
+        assertEquals("We are Ghu Kaku and our rating is 4 and our price category is $$", test1.toString());
+    }
+
+    @Test public void testRestaurantAddReview() {
+        Restaurant res1 = new Restaurant("Ghu Kaku", 4, "$$");
+        res1.addReview(new Review("good food", "Chelsea", 5));
+
+        Review first = new Review("good food", "Chelsea", 5);
+        res1.addReview(first);
+
+        Review second = new Review("ok food", "Lindsey", 2);
+        res1.addReview(second);
+
+        Review third = new Review("great food", "Shawn", 3);
+        res1.addReview(third);
+
+        Review fourth = new Review("horrible food", "Choosy", 1);
+        res1.addReview(fourth);
+
+        assertEquals(3, res1.numOfStars);
+    }
+
 }
